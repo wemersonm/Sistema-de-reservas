ea6da082b127fd0bfd65df1fa167dc191398337d
wemersonm <wemersonmoura02@gmail.com>
2023-05-27 01:01:40 -0300

    layout-with-bootstrap

--- app/Views/home.php
+++ app/Views/home.php
@@ -21,45 +21,47 @@
     </button>
 </div>
 
-<div class="container">
-    <div class="row row-cols-1 row-cols-md-2 row-cols-lg-4 py-5 g-4">
-        <div class="col text-center">
-            <div class="card" style="width: 18rem;">
-                <img src="..." class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+<div class="container-fluid ">
+    <p class="display-6 mt-5 text-center">Razões para alugar na CarReservExpress</p>
+    <p class="fs-5 text-center">Confira os nossos diferenciais</p>
+    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 d-flex justify-content-center">
+        <div class="col text-center card-spacing">
+            <div class="card h-100 w-100">
+                <img src="../assets/images/cards/card1.jpg" class="card-img-top h-100" alt="car">
+                <div class="card-body d-flex flex-column">
+                    <h5 class="card-title">Ofertas Especiais de Última Hora</h5>
+                    <p class="card-text">Economize ainda mais em sua reserva de carro com nossas ofertas especiais de última hora. Não perca a chance de obter descontos exclusivos em uma ampla seleção de veículos.</p>
+                    <a href="#" class="btn btn-danger mt-auto">Saiba mais</a>
                 </div>
             </div>
         </div>
-        <div class="col text-center">
-            <div class="card" style="width: 18rem;">
-                <img src="..." class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+        <div class="col text-center card-spacing">
+            <div class="card h-100 w-100">
+                <img src="../assets/images/cards/card1.jpg" class="card-img-top" alt="car">
+                <div class="card-body d-flex flex-column">
+                    <h5 class="card-title">Proteção Abrangente de Seguros</h5>
+                    <p class="card-text">Dirija com tranquilidade, sabendo que nossa proteção abrangente de seguros cobre danos, roubo e acidentes. Sua segurança é nossa prioridade.</p>
+                    <a href="#" class="btn btn-danger mt-auto">Saiba mais</a>
                 </div>
             </div>
         </div>
-        <div class="col text-center" >
-            <div class="card" style="width: 18rem;">
-                <img src="..." class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+        <div class="col text-center card-spacing">
+            <div class="card h-100 w-100" style="width: 18rem;">
+                <img src="../assets/images/cards/card1.jpg" class="card-img-top" alt="car">
+                <div class="card-body d-flex flex-column">
+                    <h5 class="card-title">Opções de Retirada e Devolução Flexíveis</h5>
+                    <p class="card-text">Escolha o horário mais convenientes para retirar e devolver seu carro. Oferecemos opções flexíveis para se adaptar à sua agenda e garantir uma experiência livre de estresse.</p>
+                    <a href="#" class="btn btn-danger mt-auto">Saiba mais</a>
                 </div>
             </div>
         </div>
-        <div class="col text-center">
-            <div class="card" style="width: 18rem;">
-                <img src="..." class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+        <div class="col text-center card-spacing">
+            <div class="card h-100 w-100">
+                <img src="../assets/images/cards/card1.jpg" class="card-img-top" alt="car">
+                <div class="card-body d-flex flex-column">
+                    <h5 class="card-title">Opções de Pagamento Flexíveis</h5>
+                    <p class="card-text">Escolha a forma de pagamento que melhor se adequa a você. Aceitamos cartões de crédito, débito e oferecemos opções de pagamento em parcelas para facilitar sua reserva de carro.</p>
+                    <a href="#" class="btn btn-danger mt-auto">Saiba mais</a>
                 </div>
             </div>
         </div>
@@ -67,37 +69,41 @@
 </div>
 
 
-<div class="container " id="featured-3">
-    <div class="row g-4 py-5 row-cols-1 row-cols-lg-3">
+<div class="container">
+    <p class="display-6 mt-5 text-center">Ofertas</p>
+    <p class="fs-5 text-center">Confira nossas ofertas de carros</p>
+    <div class="row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-3">
         <div class="col">
-            <div class="bg-danger bg-gradient">
-                <div style="width:100%;height:25px;"></div>
+            <div class="card h-100 w-100" style="width: 18rem;">
+                <img src="https://cdni.autocarindia.com/utils/imageresizer.ashx?n=https://cms.haymarketindia.net/model/uploads/modelimages/Hyundai-Grand-i10-Nios-200120231541.jpg" class="card-img-top" alt="carPromo">
+                <div class="card-body">
+                    <span class="badge text-bg-warning">Oferta</span>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="card-link text-danger">Alugar</a>
+                </div>
+
             </div>
-            <h2>Car Reserve Express</h2>
-            <p>Com nosso sistema de reserva de carros, você pode dizer adeus às longas filas de espera nas agências de aluguel de veículos. Desfrute da comodidade de reservar seu carro antecipadamente e retirá-lo de forma rápida e fácil em nossa agência. Basta chegar, apresentar seus documentos e pegar as chaves, sem perder tempo em filas demoradas. Aproveite cada minuto da sua viagem, sabendo que seu carro estará pronto para você, sem complicações. Simplifique sua experiência de aluguel de carros com o nosso sistema e desfrute de uma retirada sem estresse.</p>
-            <a href="#" class="icon-link">
-                Saiba mais
-            </a>
         </div>
         <div class="col">
-            <div class="bg-danger bg-gradient">
-                <div style="width:100%;height:25px;"></div>
+            <div class="card h-100 w-100" style="width: 18rem;">
+
+                <img src="https://cdni.autocarindia.com/utils/imageresizer.ashx?n=https://cms.haymarketindia.net/model/uploads/modelimages/Hyundai-Grand-i10-Nios-200120231541.jpg" class="card-img-top" alt="carPromo">
+                <div class="card-body">
+                    <span class="badge text-bg-warning">Oferta</span>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="card-link text-danger">Alugar</a>
+                </div>
             </div>
-            <h2>Frota Diversificada ao Seu Alcance</h2>
-            <p> Explore uma ampla seleção de carros disponíveis para reserva. Desde veículos compactos até SUVs luxuosos, nosso sistema oferece uma variedade de opções para atender às suas necessidades e preferências.</p>
-            <a href="#" class="icon-link">
-                Saiba mais
-            </a>
         </div>
         <div class="col">
-            <div class="bg-danger bg-gradient">
-                <div style="width:100%;height:25px;"></div>
+            <div class="card h-100 w-100" style="width: 18rem;">
+                <img src="https://cdni.autocarindia.com/utils/imageresizer.ashx?n=https://cms.haymarketindia.net/model/uploads/modelimages/Hyundai-Grand-i10-Nios-200120231541.jpg" class="card-img-top" alt="carPromo">
+                <div class="card-body">
+                    <span class="badge text-bg-warning">Oferta</span>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="card-link text-danger">Alugar</a>
+                </div>
             </div>
-            <h2>Pagamento Seguro e Conveniente</h2>
-            <p> Desfrute de pagamentos online rápidos e seguros. Nosso sistema de pagamento integrado permite que você finalize suas reservas com facilidade e tranquilidade, garantindo a proteção de suas informações financeiras.</p>
-            <a href="#" class="icon-link">
-                Saiba mais
-            </a>
         </div>
     </div>
 </div>
