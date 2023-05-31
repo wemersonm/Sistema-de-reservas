1e652b272888ab7142d2ab2ef4aaae9d1e57ea4d
wemersonm <wemersonmoura02@gmail.com>
2023-05-30 22:57:53 -0300

    remove_file_js

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,9 +1,1 @@
-function showOptions(select) {
-  for (i = 8; i < 20; i++) {
-    select.innerHTML += "<option value=>" + i + ":00" + "</option>";
-    for (j = 0; j < 1; j++) {
-      select.innerHTML += "<option>" + i + ":30" + "</option>";
-    }
-  }
-  select.innerHTML += "<option>20:00</option>";
-}
+
